1c8ae49b97625d1fc824b8f8e7a1cd7277bbba96
Petra Jaros <peeja@peeja.com>
2026-06-16 14:36:10 -0400

    refactor: `attestation.Code` → `attestation.VarsigCode` (clearer)

--- attestation/varsig.go
+++ attestation/varsig.go
@@ -11,25 +11,25 @@ import (
 func init() {
 	// Register spec-defined signature algorithms.
 	varsig.RegisterAlgorithmScheme(algorithm.AlgorithmDef{
-		Code:    Code,
+		Code:    VarsigCode,
 		Name:    "Attested Authority",
 		Decoder: DecodeAlgoithm,
 	})
 }
 
-// Code is the Varsig signature algorithm code for attested signatures, under
+// VarsigCode is the Varsig signature algorithm code for attested signatures, under
 // fil-one RFC 7. Note that the Varsig signature algorithm codes are *not*
 // Multicodec codes! Officially, the Varsig code table makes no provision for
 // extension, but we've selected a code in *Multicodec's* "private use" range,
 // on the theory that it should be safe.
-const Code uint64 = 0x300001
+const VarsigCode uint64 = 0x300001
 
 type Algorithm struct{}
 
 var algorithmInstance algorithm.Algorithm = Algorithm{}
 
 func (alg Algorithm) Encode() ([]byte, error) {
-	return varint.ToUvarint(Code), nil
+	return varint.ToUvarint(VarsigCode), nil
 }
 
 func DecodeAlgoithm(input []byte) (algorithm.Algorithm, int, error) {
@@ -37,8 +37,8 @@ func DecodeAlgoithm(input []byte) (algorithm.Algorithm, int, error) {
 	if err != nil {
 		return nil, 0, err
 	}
-	if code != Code {
-		return nil, n, fmt.Errorf("signature code is not attested-authority: 0x%02x, expected: 0x%02x", code, Code)
+	if code != VarsigCode {
+		return nil, n, fmt.Errorf("signature code is not attested-authority: 0x%02x, expected: 0x%02x", code, VarsigCode)
 	}
 	return algorithmInstance, n, nil
 }
